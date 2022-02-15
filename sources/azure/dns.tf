912ee1e4dfb650dcefabaa7413f235515650a05c
Steven O <steveno@liatrio.com>
2022-02-15 14:32:24 -0600

    fixed resource group

--- sources/azure/dns.tf
+++ sources/azure/dns.tf
@@ -5,7 +5,7 @@ data "azurerm_dns_zone" "runner_zone" {
 
 resource "azurerm_dns_cname_record" "github_webhook" {
   name                = var.webhook_domain
-  resource_group_name = data.azurerm_dns_zone.runner_zone.name
+  resource_group_name = data.azurerm_dns_zone.runner_zone.resource_group_name
   ttl                 = 300
   zone_name           = data.azurerm_dns_zone.runner_zone.name
   record              = "${var.cname_target_value}.centralus.cloudapp.azure.com"
